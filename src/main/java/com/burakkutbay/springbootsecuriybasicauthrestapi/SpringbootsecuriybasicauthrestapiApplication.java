a0fda6013725551303e21bb6a9d73baa07d5263f
BrkSe <brkktby@gmail.com>
2019-11-05 21:33:22 +0300

    Component scan eklendi.

--- src/main/java/com/burakkutbay/springbootsecuriybasicauthrestapi/SpringbootsecuriybasicauthrestapiApplication.java
+++ src/main/java/com/burakkutbay/springbootsecuriybasicauthrestapi/SpringbootsecuriybasicauthrestapiApplication.java
@@ -2,7 +2,9 @@ package com.burakkutbay.springbootsecuriybasicauthrestapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = "com.burakkutbay.springbootsecuriybasicauthrestapi")
 @SpringBootApplication
 public class SpringbootsecuriybasicauthrestapiApplication {
 
