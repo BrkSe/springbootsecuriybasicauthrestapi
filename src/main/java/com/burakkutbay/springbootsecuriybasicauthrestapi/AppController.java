27ec5c0b9699a485379ed4b1fe8d43f6bd480054
BrkSe <brkktby@gmail.com>
2019-11-05 20:54:40 +0300

    user restımpl oluşturuldu.

--- src/main/java/com/burakkutbay/springbootsecuriybasicauthrestapi/AppController.java
+++ src/main/java/com/burakkutbay/springbootsecuriybasicauthrestapi/AppController.java
@@ -1,7 +1,13 @@
 package com.burakkutbay.springbootsecuriybasicauthrestapi;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AppController {
+
+    @RequestMapping("/user")
+    public String loginUser(){
+        return "User kullanıcı girişi başarılı";
+    }
 }
