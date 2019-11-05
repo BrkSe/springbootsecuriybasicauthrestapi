6b82a1ef29030af2661c801b2bd713ba372d4112
BrkSe <brkktby@gmail.com>
2019-11-05 21:33:57 +0300

    Hatalar düzenlendi.

--- src/main/java/com/burakkutbay/springbootsecuriybasicauthrestapi/SpringSecurityConfig.java
+++ src/main/java/com/burakkutbay/springbootsecuriybasicauthrestapi/SpringSecurityConfig.java
@@ -1,35 +1,46 @@
 package com.burakkutbay.springbootsecuriybasicauthrestapi;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
+
+
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-       getHttp().httpBasic().and().authorizeRequests()
-               .antMatchers("/user").hasAnyRole("USER")
-               .antMatchers("/admin").hasAnyRole("ADMIN")
-               .and()
-               .csrf().disable();
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic().and().authorizeRequests()
+                .antMatchers("/user").hasRole("USER")
+                .antMatchers("/admin").hasRole("ADMIN")
+                .and()
+                .csrf().disable();
     }
 
     @Autowired
     public void configureAuthGlobal(AuthenticationManagerBuilder auth){
         try {
             auth.inMemoryAuthentication()
-                    .withUser("user").password("password").roles("USER")
+                    .withUser("user").password(passwordEncoder().encode("password")).roles("USER")
                     .and()
-                    .withUser("admin").password("password").roles("USER","ADMIN");
+                    .withUser("admin").password(passwordEncoder().encode("password")).roles( "ADMIN");
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 
