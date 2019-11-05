b3390fafe274bd091fa81d52066cac6dd2d04020
BrkSe <brkktby@gmail.com>
2019-11-05 20:47:52 +0300

    Initial commit

---
+++ src/main/java/com/burakkutbay/springbootsecuriybasicauthrestapi/SpringbootsecuriybasicauthrestapiApplication.java
@@ -0,0 +1,13 @@
+package com.burakkutbay.springbootsecuriybasicauthrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootsecuriybasicauthrestapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootsecuriybasicauthrestapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/burakkutbay/springbootsecuriybasicauthrestapi/SpringbootsecuriybasicauthrestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.burakkutbay.springbootsecuriybasicauthrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootsecuriybasicauthrestapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
